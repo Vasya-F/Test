e4d33587fdf00b03d085bf37fd5b677a2b125b68
Vasya-F <vasya_f@ukr.net>
2021-01-21 18:16:50 +0200

    1

--- Test.ino
+++ Test.ino
@@ -24,9 +24,60 @@ void loop()
   }
 }
 
-case MS_BTN_DOWN:
-      {
-        handleRepetitiveButton(can_msg->Data, MD_PREV_TRACK, MD_FAST_REWIND);
-        handleButton(can_msg->Data, nullptr, BT_REFUSE);
-        break;
-  
+
+
+bool isModuleInitialized;
+char serialBuffer[32];
+uint8_t serialBufferIdx = 0;
+
+void processCommand()
+{
+  if (!isModuleInitialized && (strcmp(serialBuffer, "QM+01") == 0 || strcmp(serialBuffer, "QM+02") == 0 || strcmp(serialBuffer, "QM+03") == 0))
+  {
+    isModuleInitialized = true;
+    // init code here
+    Serial3.print("Init by the command: ");
+    Serial3.println(serialBuffer);
+  }
+  else if (strcmp(serialBuffer, "CMD") == 0)
+  {
+    // another command here
+  }
+  else
+  {
+    // remove in the final code
+    Serial3.print("Unknown command: ");
+    Serial3.println(serialBuffer);
+  }
+}
+
+void setup()
+{
+  Serial3.begin(115200);
+  Serial3.println(F("Starting..."));
+}
+
+void loop()
+{
+  while (Serial1.available() > 0)
+  {
+    char c = Serial1.read();
+    if (c == '\n')
+    {
+      serialBuffer[serialBufferIdx] = '\0';
+      serialBufferIdx = 0;
+      processCommand();
+    }
+    else if (c == '\0')
+    {
+      // ignore
+    }
+    else
+    {
+      serialBuffer[serialBufferIdx] = c;
+      serialBufferIdx++;
+      if (serialBufferIdx >= sizeof(serialBuffer))
+        serialBufferIdx = 0;
+    }
+  }
+}
